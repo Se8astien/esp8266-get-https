38e438843039c7f6c8d40bb7bd0aad52f9f4a44a
Sébastien Mansion <sebastien_mansion@orange.fr>
2017-11-11 14:57:00 +0100

    Update https-get-request.ino

--- https-get-request.ino
+++ https-get-request.ino
@@ -1,1 +1,85 @@
 
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+
+const char* ssid     = "SSID;
+const char* password = "PASSWD";
+
+const char* host = "test.com;
+
+const int httpsPort = 443;
+
+// Use web browser to view and copy
+// SHA1 fingerprint of the certificate
+const char* fingerprint = "B3 F1 34 53 8F D9 84 B3 F0 06 10 64 92 EF E9 8D 1C 2E 45 E3";
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+
+  // We start by connecting to a WiFi network
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+
+void loop() {
+  delay(5000);
+
+
+  Serial.print("connecting to ");
+  Serial.println(host);
+
+  // Use WiFiClient class to create TCP connections
+   WiFiClientSecure client;
+  Serial.print("connecting to ");
+  Serial.println(host);
+  if (!client.connect(host, httpsPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+
+  if (client.verify(fingerprint, host)) {
+    Serial.println("Certificate matches");
+  } else {
+    Serial.println("Certificate doesn't match");
+  }
+
+  // We now create a URI for the request
+  String url = "/api/test";
+
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+
+  // This will send the request to the server
+  client.print(String("GET ") +url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: close\r\n\r\n");
+  delay(10);
+
+  // Read all the lines of the reply from server and print them to Serial
+  Serial.println("Respond:");
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+
+  Serial.println();
+  Serial.println("closing connection");
+}
